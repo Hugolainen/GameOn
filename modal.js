6f982cc54c330156f2e5f749f7839efff0103a02
hugoC42 <hugo.constant42@gmail.com>
2020-12-09 16:07:20 +0200

    Issue4.2: Solve in pure HTML

--- modal.js
+++ modal.js
@@ -13,6 +13,17 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll(".closeModal-btn");
 
+const formFirst = document.getElementById('first');
+const formLast = document.getElementById('last');
+const formEmail = document.getElementById('email');
+const formBirthdate = document.getElementById('birthdate');
+const formQuantity = document.getElementById('quantity');
+const formLocation = document.querySelectorAll('location');
+const formCheckbox1 = document.getElementById('checkbox1');
+const formCheckbox2 = document.getElementById('checkbox2');
+
+const formSubmit = document.querySelectorAll(".btn-submit");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -28,3 +39,19 @@ closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+function validateInput(){
+  if(true
+
+  ){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+formSubmit.addEventListener('click', ($event) => {
+  $event.preventDefault()}
+);
+  
